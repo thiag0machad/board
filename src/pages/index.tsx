181bef392ef8b4a20064ccaa944d333252a89be9
Thiago Machado <thcosta0@hotmail.com>
2022-01-12 12:31:09 -0300

    add sass

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
+import styles from '../styles/styles.module.scss';
+
 export default function Home() {
   return (
     <div>
-      <h1>Board App</h1>
+      <h1 className={styles.title}>
+        Board App<span>NextJS</span>
+      </h1>
     </div>
   );
 }
