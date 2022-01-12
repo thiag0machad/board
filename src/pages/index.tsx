539fc023a789e57182d1e33ce9de7db983c2e74c
Thiago Machado <thcosta0@hotmail.com>
2022-01-12 12:39:56 -0300

    add dynamic title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
 import styles from '../styles/styles.module.scss';
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>
-        Board App<span>NextJS</span>
-      </h1>
-    </div>
+    <>
+      <Head>
+        <title>Board - Organizando suas tarefas.</title>
+      </Head>
+      <div>
+        <h1 className={styles.title}>
+          Board App<span>NextJS</span>
+        </h1>
+      </div>
+    </>
   );
 }
